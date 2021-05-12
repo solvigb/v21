91abbba6f0c78da0edc45ed3695d5666631b36f7
solvigb <42898098+solvigb@users.noreply.github.com>
2021-05-12 15:42:18 +0000

    Add files via upload

---
+++ BillSkri_dreki1.ino
@@ -0,0 +1,5 @@
+//Skriðdreka beygjur prufa
+//Vinstri
+digitalWrite(in1, HIGH); digitalWrite(in2, LOW); digitalWrite(in3, HIGH); digitalWrite(in4, LOW); delay(2000);
+//Hægri
+digitalWrite(in1, LOW); digitalWrite(in2, HIGH); digitalWrite(in3, LOW); digitalWrite(in4, HIGH); delay(2000);
